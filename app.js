458af83e51fc9edde6cef94e4861d3ec300fd502
Ibrahim AlRayyan <ibrahim.alrayany@gmail.com>
2024-02-21 08:12:45 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+http.createServer(function (req, res) {
+	res.write("On the way to being a full stack engineer 🤖🌞");
+	res.end();
+
+}).listen(3000);
+
+console.log("Server started on port: 3000");
+
+
+
+
+
+
+
+
+
+
+
+
+
